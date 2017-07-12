fe3c6b0b367a8593287add282c5f344e33463b66
Ben Wolfe <wolfeben16@gmail.com>
2017-07-11 22:39:43 -0700

    Successful implementation of full text compression program

--- Assignment_3/zip.py
+++ Assignment_3/zip.py
@@ -3,17 +3,16 @@
 """
 
 B.Wolfe | V00205547
-SENG 265 - Assignment 3
 
 DESCRIPTION: 
-This program is phase 2 of a text compression scheme.  The program
-performs move-to-front encoding followed by run-length encoding to
-compress a text file.
+This program compresses and extracts text files using the Burrows-Wheeler transform,
+move-to-front encoding, and run-length encoding in block sizes up to 500 characters.
 
 DATE:
 11.07.2017
 
 """
+
 
 import sys
 import argparse
@@ -21,30 +20,73 @@ import struct
 from itertools import groupby
 import re
 
-ph1_magic = '\xab\xba\xbe\xef'
+
+default_blocksize = 500
 ph2_magic = '\xda\xaa\xaa\xad'
 
-def encode(infile, outfile):
+
+def compress(infile, outfile, blocksize):
+	""" 
+	Compress performs a forward text transformation then calls the required 
+	functions for move-to-front encoding and run-length encoding
+	"""
+	
+	write_magic_bytes(outfile, blocksize, ph2_magic)
+	transformed_text = []
+
+	# Attempt to open 'infile'; exit gracefully if opening is unccessful
+	try:
+		with open(infile, 'r', encoding='latin-1') as input_fp:
+			# Read the file block by block while there is something to read
+			text_buffer = input_fp.read(blocksize)
+			while text_buffer:
+				buffer_mod = text_buffer + '\x03'
+
+				# Store word rotations in a list
+				word_list = [buffer_mod[x:]+buffer_mod[:x] for x in range(len(buffer_mod))]
+			
+				# Sort list; extract last character from each word; join to create the compressed word
+				word_list.sort()
+				new_word = "".join([word[-1] for word in word_list])
+
+				transformed_text.extend(new_word)
+
+				# Read in the next stream of text
+				text_buffer = input_fp.read(blocksize)	
+	except IOError:
+		print('\nFILE ERROR\nUnable to open %s\n' % infile)
+
+	transformed_text = "".join(transformed_text)
+	encode(transformed_text, blocksize, outfile)
+
+
+
+def write_magic_bytes(outfile, blocksize, magic_bytes):
+	"""
+	Writes the first 4 file-identifying bytes to the file followed by the blocksize.
+	"""
+	# Include the identifying file bytes to file_details
+	file_details = magic_bytes
+
+	# Convert the blocksize to a string to store in file_details 
+	num_bytes = struct.pack("I", blocksize)
+	char_bytes = [chr(n) for n in num_bytes]
+	file_details += "".join(char_bytes)
+
+	# Write the file details as the first 8 bytes of the file
+	with open(outfile, 'w', encoding='latin-1') as output_fp:
+		output_fp.write(file_details)
+
+
+
+def encode(transformed_text, blocksize, outfile):
 	""" 
 	Encodes the contents of an input file using move-to-front encoding followed 
 	by run-length encoding and converts the result to an ascii string using a
 	modified scheme for numeric characters.
 	"""
-	blocksize = read_ph1_bytes(infile)
-	write_magic_bytes(outfile, blocksize, ph2_magic)
-
-	# Attempt to open 'infile'; exit gracefully if opening is unccessful
-	try:
-		with open(infile, 'r', encoding='latin-1') as input_file:
-			# Read the file as one block starting after the file details
-			input_file.seek(8)
-			buffer = input_file.read()
-
-	except IOError:
-		print('\nFILE ERROR\nUnable to open %s\n' % infile)
-		sys.exit()
-
-	mtf = mtf_encode(buffer)
+	
+	mtf = mtf_encode(transformed_text)
 	rl = rl_encode(mtf)
 	anscii = to_anscii(rl)
 
@@ -57,31 +99,7 @@ def encode(infile, outfile):
 		print('\nFILE ERROR\nUnable to write to %s\n' % outfile)
 		sys.exit()
 
-def read_ph1_bytes(infile):
-	"""
-	Reads the first 8 bytes of the text file to 1) confirm the file has the
-	4 identifying ph1 bytes and 2) to obtain the block size.
-	"""
-	try:
-		with open(infile, 'r', encoding='latin-1') as input_fp:
-			f_info = list(input_fp.read(8))
 
-			# Check that the input file is of .ph1 file type
-			if f_info[0] != '\xab' or f_info[1] != '\xba' or f_info[2] != '\xbe' or f_info[3] != '\xef':
-				print('\nFILE ERROR\nInput file is not a .ph1 file\n')
-				sys.exit();
-
-			# Aggregate the 4 number bytes to obtain the block size
-			to_ints = [ord(num) for num in f_info[4:]]
-			to_bytes = bytearray(to_ints)
-			blocksize = struct.unpack("I", to_bytes)
-			blocksize = blocksize[0]
-			
-	except IOError:
-		print('\nFILE ERROR\nUnable to open %s\n' % infile)
-		sys.exit()
-
-	return blocksize
 
 def mtf_encode(buf):
 	"""
@@ -107,6 +125,8 @@ def mtf_encode(buf):
 
 	return encoding
 
+
+
 def rl_encode(buf):
 	"""
 	Groups repated elements together and performs an aggregation when the repeated
@@ -125,6 +145,9 @@ def rl_encode(buf):
 			encoding.extend(group)
 
 	return encoding
+
+
+
 
 def to_anscii(buf):
 	"""
@@ -145,7 +168,10 @@ def to_anscii(buf):
 	anscii_string = "".join(anscii_list)
 	return anscii_string
 
-def decode(infile, outfile):
+
+
+
+def extract(infile, outfile):
 	"""
 	Decodes the contents of a .ph2 file by first converting the original string using 
 	the custom anscii scheme identified above. Run length decode and move to front decode
@@ -153,7 +179,6 @@ def decode(infile, outfile):
 	"""
 
 	blocksize = read_ph2_bytes(infile)
-	write_magic_bytes(outfile, blocksize, ph1_magic)
 
 	# Attempt to open 'infile'; Exit gracefully if uncessful
 	try:
@@ -168,14 +193,11 @@ def decode(infile, outfile):
 
 	alpha_num = from_anscii(buffer) # The results from ascii contain alphabetic and numeric chars
 	rl = rl_decode(alpha_num)
-	mtf = mtf_decode(rl)
-	
-	try:
-		with open(outfile, 'a', encoding='latin-1') as output_file:
-			output_file.write(mtf)
-	except IOError:
-		print("\nFILE ERROR\nUnable to open %s\n" %outfile)
-		sys.exit()
+	rotated_text = mtf_decode(rl)
+
+	reverse_transform(rotated_text, blocksize, outfile)
+
+
 
 def read_ph2_bytes(infile):
 	"""
@@ -202,6 +224,9 @@ def read_ph2_bytes(infile):
 
 	return blocksize
 
+
+
+
 def from_anscii(buf):
 	"""
 	Converts a string to its alpha-numeric equivalent using a modified scheme for numeric
@@ -221,6 +246,9 @@ def from_anscii(buf):
 
 	return alpha_num_list
 
+
+
+
 def rl_decode(buf):
 	"""
 	Uncompresses the buffer by adding the correct number of 1s identified by the digit after 0
@@ -238,6 +266,8 @@ def rl_decode(buf):
 				decoding.append(char)
     
 	return decoding
+
+
 
 def mtf_decode(buf):
 	"""
@@ -274,68 +304,63 @@ def mtf_decode(buf):
 	decoded_str = "".join(decoding)
 	return decoded_str
 
-def write_magic_bytes(outfile, blocksize, magic_bytes):
-	"""
-	Writes the first 4 file-identifying bytes to the file followed by the blocksize.
-	"""
-	# Include the identifying file bytes to file_details
-	file_details = magic_bytes
 
-	# Convert the blocksize to a string to store in file_details 
-	num_bytes = struct.pack("I", blocksize)
-	char_bytes = [chr(n) for n in num_bytes]
-	file_details += "".join(char_bytes)
 
-	# Write the file details as the first 8 bytes of the file
+def reverse_transform(rotated_text, blocksize, outfile):
+	"""
+	Performs the reverse text transformation to get back to the original text file.
+	"""
+	
+	# Clear the contents of the output file (preventative measure if the file already exists)
 	with open(outfile, 'w', encoding='latin-1') as output_fp:
-		output_fp.write(file_details)
+		output_fp.write("")
+				
+	# Read the text 'blocksize+1' characters at a time to account for the '\x03' symbol
+	marker = 0
+	while marker < len(rotated_text):
+		
+		# Update the text buffer
+		original = list(rotated_text[marker : (marker+blocksize+1)])
+		transfer_counter = 0
+		
+		while transfer_counter < (len(original)-1):	
+			# Copy the contents of the original list to a temporary list and sort
+			temporary = list(original)
+			temporary.sort()
 
-def read_command_line():
-	# Create the argument parser object
-	parser = argparse.ArgumentParser(description='A program for text compression and extraction.')
+			# Add last char of each element in the temp list to original list
+			original = [original[x]+temporary[x][-1] for x in range(len(original))]
+			transfer_counter += 1
 
-	# Specify mutually exclusive arguments
-	coding = parser.add_mutually_exclusive_group(required=True)
-	coding.add_argument('--encode', action='store_true', help='specify for compression')
-	coding.add_argument('--decode', action='store_true', help='specify for extraction')
+		# Search for EOT symbol and retrieve corresponding word
+		uncomp_string = [original[x][:-1] for x in range(len(original)) if original[x][-1] == '\x03']
 
-	# Specify additional arguments
-	parser.add_argument('--infile', type=str, help='input text file', required=True)
-	parser.add_argument('--outfile', type=str, help='stores program output', required=True)
+		# Append the contents to outfile
+		with open(outfile, 'a', encoding='latin-1') as output_fp:
+			output_fp.write(uncomp_string[0])
 
-	# Parse and check the arguments
-	args = parser.parse_args()
-	check_arguments(args)
-	
-	# Use transformation direction to select program mode 
-	if args.encode:
-		encode(args.infile, args.outfile)
-		return
+		# Update the start of string marker 
+		marker += blocksize+1
 
-	elif args.decode:
-		decode(args.infile, args.outfile)
-		return
-	
-	else:
-		sys.exit()
+
 
 def check_arguments(args):
 	"""
 	Checks the input file names against regular expressions to make sure the extensions are
-	.ph1 and .ph2.
+	.txt and .ph2.
 	"""
 
-	if args.encode:
-		f_in_pat = r'.+.ph1\b'
+	if args.compress:
+		f_in_pat = r'.+.txt\b'
 		f_out_pat = r'.+.ph2\b'
-		in_ext = '.ph1'
+		in_ext = '.txt'
 		out_ext = '.ph2'
 
-	elif args.decode:
+	elif args.extract:
 		f_in_pat = r'.+.ph2\b'
-		f_out_pat = r'.+.ph1\b'
+		f_out_pat = r'.+.txt\b'
 		in_ext = '.ph2'
-		out_ext = '.ph1'
+		out_ext = '.txt'
 
 	if not re.match(f_in_pat, args.infile) or not re.match(f_out_pat, args.outfile):
 		print("\nFILE ERROR\nThe following files have invalid extensions:")
@@ -349,8 +374,46 @@ def check_arguments(args):
 		print()
 		sys.exit()
 
+
+def read_command_line():
+	# Create the argument parser object
+	parser = argparse.ArgumentParser(description='A program for text compression and extraction.')
+
+	# Specify mutually exclusive arguments
+	coding = parser.add_mutually_exclusive_group(required=True)
+	coding.add_argument('--compress', action='store_true', help='specify for compression')
+	coding.add_argument('--extract', action='store_true', help='specify for extraction')
+
+	# Specify additional arguments
+	parser.add_argument('--infile', type=str, help='input text file', required=True)
+	parser.add_argument('--outfile', type=str, help='compressed output file', required=True)
+	parser.add_argument('--blocksize', type=int, help='select the blocksize for compression')
+
+	# Parse and check the arguments
+	args = parser.parse_args()
+	check_arguments(args)
+	
+	# Use transformation direction to select program mode 
+	if args.compress:
+		# Use the user specified blocksize otherwise use the default
+		if args.blocksize:
+			blocksize = args.blocksize
+		else:
+			blocksize = default_blocksize
+		compress(args.infile, args.outfile, blocksize)
+		return
+
+	elif args.extract:
+		extract(args.infile, args.outfile)
+		return
+	
+	else:
+		sys.exit()
+
+
 def main():
 	read_command_line()
+
 
 if __name__ == '__main__':
 	main()
